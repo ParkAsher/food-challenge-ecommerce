de5f76d5a9172abc8858b4dddb9e9c5aa334924f
ParkAsher <devasherpark@naver.com>
2023-02-02 16:24:03 +0900

    feat: 입력받은 이메일에 해당하는 회원찾기

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -1,3 +1,20 @@
-class UserRepository {}
+class UserRepository {
+    constructor(UserModel) {
+        // DI
+        this.userModel = UserModel;
+    }
+
+    findUser = async (userInfo) => {
+        try {
+            const user = await this.userModel.findOne({
+                where: { email: userInfo.email },
+            });
+
+            return user;
+        } catch (error) {
+            throw error;
+        }
+    };
+}
 
 module.exports = UserRepository;
