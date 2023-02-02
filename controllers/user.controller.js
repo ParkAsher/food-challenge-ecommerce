35b06390f94d1e979dba2b528206a6cb7086059f
ParkAsher <devasherpark@naver.com>
2023-02-02 14:37:19 +0900

    feat: 회원가입 회원데이터 유효성검사 양식

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,21 @@
-class UserController {}
+const bcrypt = require('bcrypt');
+const UserService = require('../services/user.service');
+
+/* joi */
+const { userRegisterDataValidate } = require('../lib/joischema');
+
+class UserController {
+    userService = new UserService();
+
+    register = async (req, res, next) => {
+        try {
+            const userInfo = await userRegisterDataValidate.validateAsync(req.body);
+        } catch (error) {
+            console.log(error.isJoi);
+            console.log(error);
+            next(error);
+        }
+    };
+}
 
 module.exports = UserController;
