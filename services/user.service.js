32ddde94ec472ee2e5f51ac93ea652958e2c8000
ParkAsher <devasherpark@naver.com>
2023-02-02 16:39:05 +0900

    feat: 비밀번호 암호화

--- services/user.service.js
+++ services/user.service.js
@@ -1,3 +1,29 @@
-class UserService {}
+const bcrypt = require('bcrypt');
+const UserRepository = require('../repositories/user.repository');
+const { User } = require('../models/index');
+
+/* custom error */
+const { UserAlreadyExist } = require('../lib/customerror');
+
+class UserService {
+    userRepository = new UserRepository();
+
+    register = async (userInfo) => {
+        try {
+            // Email 중복 체크
+            const user = await this.userRepository.findUser(userInfo);
+            // 해당 Email을 가진 회원이 존재
+            if (user) {
+                const error = new UserAlreadyExist();
+                throw error;
+            }
+
+            // 비밀번호 암호화
+            const hashedPassword = await bcrypt.hash(userInfo.password, 10);
+        } catch (error) {
+            throw error;
+        }
+    };
+}
 
 module.exports = UserService;
