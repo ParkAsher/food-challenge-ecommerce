3051a54545e0c78c810370f342e2b963c74d50e7
ParkAsher <devasherpark@naver.com>
2023-02-03 17:42:23 +0900

    feat: 관리자 페이지 메인 버튼 디자인

--- routes/ejs.routes.js
+++ routes/ejs.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { NotAdmin } = require('../lib/customerror');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 /* View Mapping */
 router.get('/', (req, res, next) => {
@@ -12,6 +14,28 @@ router.get('/register', (req, res, next) => {
 
 router.get('/items', (req, res, next) => {
     res.render('index.ejs', { components: 'oneItem' });
+});
+
+/* 관리자 */
+// router.get('/adm', auth, (req, res, next) => {
+//     try {
+//         if (res.locals.user.id === '1' && res.locals.user.nickname === '관리자') {
+//             res.render('admin_index');
+//         } else {
+//             const error = new NotAdmin();
+//             throw error;
+//         }
+//     } catch (error) {
+//         throw error;
+//     }
+// });
+/* 테스트 */
+router.get('/adm', (req, res, next) => {
+    try {
+        res.render('admin_index');
+    } catch (error) {
+        throw error;
+    }
 });
 
 module.exports = router;
