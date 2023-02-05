39217d67518e90bd2bcc99949384e4502b0245ac
Joominseok <jumseok12@gmail.com>
2023-02-05 15:27:30 +0900

    잠시만용...

---
+++ repositories/admin.repository.js
@@ -0,0 +1,70 @@
+class AdminRepository {
+    constructor(UserModel) {
+        // DI
+        this.userModel = UserModel;
+    }
+
+    allUsersCount = async () => {
+        try {
+            return await this.userModel.count();
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    getAllUsers = async (page) => {
+        try {
+            return await this.userModel.findAll({
+                offset: (page - 1) * 8,
+                limit: 8,
+                order: [['id', 'DESC']],
+            });
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    searchUser = async (email) => {
+        try {
+            return await this.userModel.findOne({
+                where: { email },
+            });
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    deleteUser = async (userInfo) => {
+        try {
+            return await this.userModel.destroy({
+                where: {
+                    id: userInfo.id,
+                    email: userInfo.email,
+                },
+            });
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    updateUser = async (userInfo) => {
+        try {
+            return await this.userModel.update(
+                {
+                    name: userInfo.name,
+                    nickname: userInfo.nickname,
+                    email: userInfo.email,
+                    phone: userInfo.phone,
+                    point: userInfo.point,
+                },
+                {
+                    where: { id: userInfo.id },
+                }
+            );
+        } catch (error) {
+            throw error;
+        }
+    };
+}
+
+module.exports = AdminRepository;
