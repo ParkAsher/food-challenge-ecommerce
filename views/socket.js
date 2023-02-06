4c8fb96cc657d92a34ec2cbfa9d7bb48883e3f85
Younghwan90s <younghwan1608@gmail.com>
2023-02-06 20:17:37 +0900

    feature/socket - io handshake position change

--- views/socket.js
+++ views/socket.js
@@ -1,26 +1,28 @@
-    
-    function socketOrderAlert(order_id, receipt_price) {
-        socket.emit('PURCHASE', {
-            order_id,
-            receipt_price,
-        });
-    }
+const socket = io.connect("/");
 
-    socket.on('PURCHASE_ALERT', function (data) {
-        const { order_id, receipt_price, date } = data;
-        purchaseNotification(order_id, receipt_price, date);
+function socketOrderAlert(order_id, receipt_price) {
+    socket.emit('PURCHASE', {
+        order_id,
+        receipt_price,
     });
+}
 
-    function purchaseNotification(order_id, receipt_price, date) {
-        const msgHtml = `총 결제 금액 ${receipt_price} 주문 번호 ${order_id}를 구매하였습니다.<small>(${date})</small>
+socket.on('PURCHASE_ALERT', function (data) {
+    const { order_id, receipt_price, date } = data;
+    purchaseNotification(order_id, receipt_price, date);
+});
+
+function purchaseNotification(order_id, receipt_price, date) {
+    const msgHtml = `총 결제 금액 ${receipt_price} 주문 번호 ${order_id}를 구매하였습니다.<small>(${date})</small>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>`;
-        const alt = $('#customerAlert');
-        if (alt.length) {
-            alt.html(msgHtml);
-        } else {
-            const htmlTemp = `<div class="alert alert-sparta" role="alert" id="customerAlert">${msgHtml}</div>`;
-            $('body').append(htmlTemp);
-        }
+
+    const alt = $('#customerAlert');
+    if (alt.length) {
+        alt.html(msgHtml);
+    } else {
+        const htmlTemp = `<div class="alert alert-sparta" role="alert" id="customerAlert">${msgHtml}</div>`;
+        $('body').append(htmlTemp);
     }
+}
